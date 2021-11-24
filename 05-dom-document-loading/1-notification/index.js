cfcf9dd074959fff8e6a9fec4fec395f0ace99a5
Dan Shadrin <shadrin.smr@gmail.com>
2021-11-24 18:41:40 +0400

    05-dom-document-loading: Решение без static element с поиском по DOM

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,61 @@
 export default class NotificationMessage {
+  element;
 
+  constructor(
+    message = '',
+    {
+      type = 'success',
+      duration = 0
+    } = {}) {
+    this.message = message;
+    this.type = type;
+    this.duration = duration;
+    this.initElement();
+  }
+
+  initElement() {
+    if (document.getElementById('notification')) {
+      document.getElementById('notification').remove();
+    }
+    const element = document.createElement('div');
+    element.innerHTML = this.template;
+    this.element = element.firstElementChild;
+  }
+
+  show(cover) {
+    if (cover) {
+      cover.innerHTML = this.element.outerHTML;
+      document.body.append(cover);
+    } else {
+      document.body.append(this.element);
+    }
+    setTimeout(() => {
+      this.remove();
+    }, this.duration);
+  }
+
+  get template() {
+    return `
+      <div id = "notification" class="notification ${this.type}" style="--value:${this.duration / 1000}s">
+        <div class="timer"></div>
+        <div class="inner-wrapper">
+          <div class="notification-header">${this.type}</div>
+          <div class="notification-body">
+              ${this.message}
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    this.remove();
+    this.element = null;
+  }
 }
